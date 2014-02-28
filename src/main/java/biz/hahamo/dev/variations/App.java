4e8b4b1512af1537e5336cded664075c13de7225
Gábor László Hajba <gabor.hajba@gmail.com>
2014-02-28 10:29:41 +0100

    liquibase from java app -- Part I
    first part of the liquibase changes from the java application: at
    startup the change sets gets running

--- src/main/java/biz/hahamo/dev/variations/App.java
+++ src/main/java/biz/hahamo/dev/variations/App.java
@@ -1,13 +1,45 @@
 package biz.hahamo.dev.variations;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import liquibase.Liquibase;
+import liquibase.database.core.H2Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
 /**
  * Hello world!
- *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    private static final String url = "jdbc:h2:file:../db/testdb";
+    private static final String username = "sa";
+    private static final String password = "";
+    private static final String changelog = "dbchange/master.xml";
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        try
+        {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            H2Database databaseConnection = new H2Database();
+            databaseConnection.setConnection(jdbcConnection);
+            Liquibase liquibase = new Liquibase(changelog, new ClassLoaderResourceAccessor(), databaseConnection);
+            liquibase.update(null);
+        }
+        catch (LiquibaseException e)
+        {
+            e.printStackTrace();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println("Hello World!");
     }
 }
